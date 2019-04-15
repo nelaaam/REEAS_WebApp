f617437f940187ee284e6dc830c3a031b4aa0c38
nelaaam <nelamartino@gmail.com>
2019-04-15 14:40:02 +0000

    fixed bug on process spawning

--- modules/checkStation.js
+++ modules/checkStation.js
@@ -5,26 +5,24 @@ const updateStation= "./modules/updateStation";
 setInterval(checkStations, 60000);
 
 function checkStations() {
-    console.log("Running every one minute");
-    let update = child.fork(updateStation);
+    console.log("Checking Stations activity every 2 mins");   
     db.getConnection((err, conn) => {
         if (err) throw err;
         var sql1 = "SELECT datetime, station FROM Stations WHERE enabled = 1";
         conn.query(sql1, (err, res) => {
             if (err) throw err;
             if (res.length > 0) {
-console.log(new Date());
-console.log(res);
                 var now = new Date();
                 for(var i = 0; i < res.length; i++){
                     var before = new Date(res[i].datetime);
                     console.log(now - before);
-                    if ((now - before) > 605000) {
+                    if ((now - before) > 602000) {
                         const schema = {
                             station: res[i].station,
                             datetime: now,
                             enabled: 0
                         }
+			let update = child.fork(updateStation);
                         update.send(schema);
                     } 
                 }
